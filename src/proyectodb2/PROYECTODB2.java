7b7118cffbfd0e8f023f150d4b4ddcb4f0333468
ACER <ACER@user>
2020-08-06 12:37:32 -0500

    empezando

---
+++ src/proyectodb2/PROYECTODB2.java
@@ -0,0 +1,18 @@
+
+package proyectodb2;
+
+/**
+ *
+ * @author ACER
+ */
+public class PROYECTODB2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=2;
+    }
+    
+}
